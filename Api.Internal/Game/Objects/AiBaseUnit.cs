4598ddf87375c5e9e6969ca9f4fd8982472f91af
koelion <koelionekpl@gmail.com>
2023-11-16 20:10:00 +0100

    fix error

--- Api.Internal/Game/Objects/AiBaseUnit.cs
+++ Api.Internal/Game/Objects/AiBaseUnit.cs
@@ -39,7 +39,7 @@ internal class AiBaseUnit : AttackableUnit, IAiBaseUnit
         }
         else
         {
-            aattackSpeed = baseAttackSpeed * (1.0f + BonusAttackSpeed);
+            attackSpeed = baseAttackSpeed * (1.0f + BonusAttackSpeed);
         }
         
         return MathF.Round(attackSpeed * 1000.0f) / 1000.0f;
