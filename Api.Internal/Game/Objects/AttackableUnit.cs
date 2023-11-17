b58ab267909db2b0aeba73edeff75272b1d3713d
koelion <koelionekpl@gmail.com>
2023-11-17 16:52:32 +0100

    Percent in 0-100 range

--- Api.Internal/Game/Objects/AttackableUnit.cs
+++ Api.Internal/Game/Objects/AttackableUnit.cs
@@ -9,10 +9,10 @@ internal class AttackableUnit : GameObject, IAttackableUnit
     public bool IsDead { get; set; }
     public float Mana { get; set; }
     public float MaxMana { get; set; }
-    public float ManaPercent => Mana / MaxMana;
+    public float ManaPercent => (Mana / MaxMana) * 100;
     public float Health { get; set; }
     public float MaxHealth { get; set; }
-    public float HealthPercent => Health / MaxHealth;
+    public float HealthPercent => (Health / MaxHealth) * 100;
     public float Armor { get; set; }
     public float BonusArmor { get; set; }
     public float TotalArmor => Armor + BonusArmor;
